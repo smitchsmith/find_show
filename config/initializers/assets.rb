73c958b651facba98378d0487da0e1c7642ebba5
smitchsmith <github@mitchellzsmith.com>
2017-01-28 13:02:42 -0500

    Styles email digest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( hack-0.5.0.css digest_mailer.css )
